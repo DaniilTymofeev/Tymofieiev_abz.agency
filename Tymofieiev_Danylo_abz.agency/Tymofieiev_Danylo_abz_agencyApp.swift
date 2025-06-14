c7313e336dc3a082e3e8121dbfa717b10965fe5a
Daniil Tymofeev <tymofeevdaniil.work@gmail.com>
2025-06-14 16:17:34 +0300

    First commit
    Init APP

---
+++ Tymofieiev_Danylo_abz.agency/Tymofieiev_Danylo_abz_agencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tymofieiev_Danylo_abz_agencyApp.swift
+//  Tymofieiev_Danylo_abz.agency
+//
+//  Created by Danil Tymofeev on 14.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct Tymofieiev_Danylo_abz_agencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Tymofieiev_Danylo_abz.agencyTests/Tymofieiev_Danylo_abz_agencyTests.swift
@@ -0,0 +1,17 @@
+//
+//  Tymofieiev_Danylo_abz_agencyTests.swift
+//  Tymofieiev_Danylo_abz.agencyTests
+//
+//  Created by Danil Tymofeev on 14.06.2025.
+//
+
+import Testing
+@testable import Tymofieiev_Danylo_abz_agency
+
+struct Tymofieiev_Danylo_abz_agencyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
